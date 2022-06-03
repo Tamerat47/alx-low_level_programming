1d4831ac1484967d293f322ca71597567d485228
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-03 14:59:11 -0700

    5.Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints decimals.
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+		printf(a);
+	printf('\n')
+return 0;
+}
