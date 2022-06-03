602cac97c2fc84ef3d96605b5d810476c8111360
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-03 16:25:32 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Prints a hexadecimal string
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+	int a;
+	for (a = '0'; a <= '9';)
+	{
+	putchar(a);
+	if (a == '9')
+		a++;
+	{
+		putchar(',',' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
