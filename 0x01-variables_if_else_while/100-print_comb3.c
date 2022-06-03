44c4b55436c7c9869a2e47f06b4c9c59869db297
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-04 02:57:51 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,18 @@ int main(void)
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
 			{
-				if (i < j)
-				{
 				putchar(i);
 				putchar(j);
-					if (i != '8' || (i == '8' && j != '9'))
-					{
+				if (i != '8' || (i == '8' && j != '9'))
+				{
 					putchar(',');
 					putchar(' ');
-					}
 				}
 			}
-		
+		}
 	}
 	putchar('\n');
 	return (0);
