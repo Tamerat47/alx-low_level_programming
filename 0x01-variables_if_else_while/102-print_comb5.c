e19ef88dffd0f775cfc5f2906b1a9fbd29ce37d1
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-04 13:59:03 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print two two-digit numbers
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int a = 0; a <= 99; a++)
+		{
+		if (a <= 99)
+			{
+			putchar((char)a / 10 + '0');
+			putchar((char)a % 10 + '0');
+			putchar(' ');
+			}
+		}
+	return (0);
+}
