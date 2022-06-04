969897bafe2b8b9d4dc818f5197d1970f4aed13e
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-04 03:22:54 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints 3 combination of numbers
- *     *
- *       * Return: Always (Success)
- *         */
+ * main - Prints 3 digits combination of numbers
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-int i, j, k;
+	int i, j, k;
 
 	for (i = '0'; i <= '9'; i++)
 	{
@@ -16,20 +16,20 @@ int i, j, k;
 			for (k = '0'; k <= '9'; k++)
 			{
 				if (i < j && j < k)
-				{	
-				putchar(i);
-				putchar(j);
-				putchar(k);
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
 				if (i != '7')
-				{	
-				putchar(',');
-				putchar(' ');
+				{
+					putchar(',');
+					putchar(' ');
+				}
 				}
 			}
-		}		
-	}	
-}
-putchar('\n');
-return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
