c748ff14543ef48531f7862e318f3c4ea343ac5b
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-05 18:33:07 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,13 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-		putchar((char)a / 10 + '0');
-		putchar((char)a % 10 + '0');
-		putchar(' ');
-		putchar((char)b / 10 + '0');
-		putchar((char)b % 10 + '0');
+			{
+			putchar((char)a / 10 + '0');
+			putchar((char)a % 10 + '0');
+			putchar(' ');
+			putchar((char)b / 10 + '0');
+			putchar((char)b % 10 + '0');
+			}
 			while (a == 98 && b == 99);
 			putchar(',');
 			putchar(' ');
