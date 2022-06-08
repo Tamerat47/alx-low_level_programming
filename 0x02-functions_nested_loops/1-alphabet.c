896bf405cc38b04c7bb6fbbf585bed312a49aa6c
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-08 18:52:30 +0300

    1-alphabet.c
    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+* main - prints the alphabet
+* Return: always 0.
+*/
+int main(void)
+{
+  char x;
+  x ='a';
+  for (x = 97; x <= 122;x++)
+    putchar(x);
+  	putchar('\n');
+  return (0);
+}
