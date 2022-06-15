408594913ec48c8c9c407b9e44e0c44c9d9ffc45
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-16 02:01:52 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,7 @@ void rev_string(char *s)
 	for (j = 0; s[j] != '\0'; j++)
 	{
 		_putchar(s[j]);
-		break;
 	}
-	
 	for (i = 0; s[i] != '\0';)
 	{
 		i++;
@@ -20,6 +18,6 @@ void rev_string(char *s)
 	for (i -= 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
