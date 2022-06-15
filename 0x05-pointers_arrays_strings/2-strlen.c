c0b64764bc6bc7df7be0338f1586ae135b31a378
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-15 16:54:36 +0300

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strlen - length of a string
+ *@s: pointer to an integer it will be updated
+ *Return: always (success)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	return (i);
+}
