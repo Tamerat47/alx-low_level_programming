6ecd7f35ab13f5540388af7b708b43af481fbd45
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-15 19:47:38 +0300

    3.puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ * @str: string
+ * Return: (success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
