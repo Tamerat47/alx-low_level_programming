0741d398161329f5505d5437172df5e5f8bff380
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-17 00:16:45 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 	while (src[i++])
 		j++;
 	while (src[i] && i < n)
-		i++;
 		dest[i] = src[i];
+		i++;
 	for (i = j; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
