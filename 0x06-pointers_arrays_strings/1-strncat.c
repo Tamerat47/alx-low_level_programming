63950be6bae817c7b2c930337d4334d0dc9399bb
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-16 22:16:11 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j;
 
 	while (dest[i++])
-	{
 		j++;
-	}
 	for (i = 0; src[i] && i < n; i++)
 	{
-		dest[j] = src[i];
-		j++;
+		dest[j++] = src[i];
 	}
 	return (dest);
 }
