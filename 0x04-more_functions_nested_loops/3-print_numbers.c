fecc7d461d78cd01bbc5ba3738d22f7927117a82
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-18 23:04:02 +0300

    single digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print number 0-9
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	return (0);
+}
