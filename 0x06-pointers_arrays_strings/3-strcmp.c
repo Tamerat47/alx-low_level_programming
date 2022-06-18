948b384ebe54724dfed983f632e11f347a7820ab
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-18 03:04:06 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (s1 == s2 && s1 != '\0' || s2 != '\0')
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+		i++;
 	{
-		s1++;
-		s2++;
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		if (s1[i] < s2[i])
+			return (s2[i] - s1[i];
 	}
-	return (*s1 - *s2);
-}
+	return (0);
