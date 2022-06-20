0ae79057be874dd8277a0b3275b9d4faf5682f71
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-20 16:06:57 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - a function that fills the first memory area
+ * @s: pointer
+ * @b: char
+ * @n: unsigned int of the frist integer
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
