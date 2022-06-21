74b556b68228724c9ad01f6482e21fd81970bfda
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-21 13:04:49 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,11 +12,10 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i]; i++)
 	{
-		for (i = 0; accept[j]; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			while (s[i] == accept[j])
 			break;
-			
 		}
 	while (s[i] != accept[j])
 	break;
