2c3d60e0692b22e78bfa743ea5ac4d8b0dc2692c
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-21 22:27:59 +0300

    fix hit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - a function that prints the chessboard
+ * @a: array of string
+ *
+ * Return: always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		while (y < 8)
+		{
+			_putchar(i[x][y]);
+		}
+	_putchar('\n');
+	}
+}
