360f3456969c4e2910198e67e0ffbf3046152303
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-22 00:45:05 +0300

    matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,10 +10,10 @@ void print_diagsums(int *a, int size)
 	int i = 0, s1 = 0, s2 = 0;
 
 	while (i < size)
+		i++;
 	{
 		s1 += a[(size + 1) * i];
 		s2 += a[(sizse - 1) * (i + 1)];
-		i++;
 	}
 	_putchar("%d,%d\n", s1, s2);
 }
