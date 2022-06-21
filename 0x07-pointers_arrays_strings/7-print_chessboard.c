d6d6834d006f2b9c3a2f85f4a1db405375a80ca9
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2022-06-21 23:31:08 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j = 0;
+	int i, j = 0;
 
-	if (i < 8)
+	for (i = 0; i < 8; i++)
 	{
-		i++;
-		if (j < 8)
+		while (j < 8)
 		{
-			j++;
 			_putchar(a[i][j]);
+			j++;
 		}
 	_putchar('\n');
 	}
