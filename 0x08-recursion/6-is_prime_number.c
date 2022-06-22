fff768febb1b7074c76903f0b7cbc9155af9b91f
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-23 01:39:01 +0300

    lough

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,9 +6,11 @@
  */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0 || n == 1 || n == 0)
+	if (n < 0)
 		return (0);
-	if (n % 3 == 0 && n < 0)
+	else if (n % 2 == 0 || n == 1 || n == 0)
+		return (0);
+	else if (n % 3 == 0)
 		return (0);
 	else
 		return (1);
