0bd145bda226be7903b88b6abadb5faec236a200
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-23 01:28:01 +0300

    tired

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_prime_number - prime number take 1 otherwise 0
+ * @n: the number integer
+ * Return: always 1 and 0
+ */
+int is_prime_number(int n);
+{
+	if (n % 2 == 0 || n == 1 || n == 0)
+		return (o);
+	if (n % 3 == 0)
+		return (0);
+	else
+		return (1);
+}
