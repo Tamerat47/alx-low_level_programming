32fa81dfd49f64251da0215376278ef4622f2a1a
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-22 23:20:36 +0300

    sqrt of number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - a funtin that returns the square root of num
+ * @n: integer
+ *
+ * Return: n result
+ */
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	if (n / x == x && x * x == n)
+	{
+		x = _sqrt_recursion(n));
+		return (x);
+	}
+	else
+		return (-1);
+}
