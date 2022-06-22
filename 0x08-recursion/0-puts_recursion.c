2d1983255810d4a97d18c816691e6905ea38d8b9
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-22 20:29:22 +0300

    recone

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@ void _puts_recursion(char *s)
 {
 	int i;
 
-	while (s[i] != 0;)
+	while (s[i] != 0)
 	{
 		_putchar(*s);
 		i++;
-		_puts_recursion(s)
+		_puts_recursion(s);
 	}
 	_putchar('\n');
 }
