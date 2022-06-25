7e4d188a05d6123b172fd68fede884802db13f2e
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-26 00:35:08 +0300

    multi calcu

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,13 +15,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
-	while (argc == 3)
-	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		c = a * b;
-	}
+
 		printf("%d\n", c);
 		return (0);
 }
