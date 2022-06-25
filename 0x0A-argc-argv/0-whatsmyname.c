fdebd6231b16f0971e2b4d144547d4e6ff4e2d5d
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-25 19:51:36 +0300

    qwert

---
+++ 0x0A-argc-argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+/**
+ * main - a funciton that print its name
+ * @argc: number of arguments
+ * @argv: a pointer to the arguments
+ * Return: always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv);
+	return (0);
+}
