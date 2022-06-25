ecf3d7eb172983b85304779560adb8820ac041ed
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-26 00:44:09 +0300

    additin argu

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function that print multiplication of two int
+ * @argc: number of arguments
+ * @argv: pointer of arguments
+ * Return: alway 1
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int a, b, c;
+
+	while (argc != 3)
+	{
+		printf("0\n");
+	}
+	while (argc == char)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a + b;
+
+		printf("%d\n", c);
+		return (0);
+}
