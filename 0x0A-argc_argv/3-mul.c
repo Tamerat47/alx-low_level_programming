422023d2bcf25879fcfd51a63f05e96aa8303284
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-26 00:30:42 +0300

    multiplying machine

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function that print multiplication of two int
+ * @argc: number of arguments
+ * @argv: pointer of arguments
+ * Return: alway 1
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	while (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	while (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+	}
+		printf("%d\n", c);
+		return (0);
+}
