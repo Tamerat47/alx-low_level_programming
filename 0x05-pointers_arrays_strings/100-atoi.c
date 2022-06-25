8d5ccbefd91fb78d476d7f8d58f636c33475fd89
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-25 14:15:01 +0300

    mine

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,17 @@ int _atoi(char *s)
 {
 	int x = 1, z;
 	unsigned int y = 0;
+	
+	do {
+		if (*s == '-')
+			x *= -1;
 
-	if (*s == '-')
-		x *= -1;
+		else if (*s >= '0' && *s <= '9')
+			y = (y * 10) + (*s - '0');
 
-	else if (*s >= '0' && *s <= '9')
-		y = (y * 10) + (*s - '0');
-
+		else if  (y > 0)
+			break;
+	}
 	while (*s++)
 
 	z = (x * y);
