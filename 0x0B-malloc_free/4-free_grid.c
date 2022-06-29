60fa7a519c7efeb2e35b4afea8a657fd1e292cde
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-30 00:45:21 +0300

    free up memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - frees a 2 dimensional grid created by your alloc_grid
+  * @grid: array of int 
+  * @height: height of grid
+  *
+  */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+	}
+	i++;
+	free(grid);
+}
