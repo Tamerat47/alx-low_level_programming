7d12a91ad6c6731fb01e51a5d385a94492d6de07
Tamerat47 <tameratmekonnen70@gmail.com>
2022-06-30 23:33:35 +0300

    fix arguments

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,12 +14,12 @@ int *array_range(int min, int max)
 	while (min > max)
 		return (NULL);
 
-	a = malloc(sizeof(int) * (max - min);
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
 
 	while (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < (max - min); i++)
+	for (i = 0; min <= max; i++)
 	{
 		a[i] = min;
 		min++;
