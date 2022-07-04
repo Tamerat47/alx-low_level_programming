db998f23c55815c6e69e0cbc893d485bd17002de
tamerat47 <tameratmekonnen70@gmail.com>
2022-07-05 01:09:40 +0300

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		(*dog).name = name;
+		(*dog).age = age;
+		(*dog).owner = owner;
 	}
 }
