d6b0362721b8db4ea71e429c273736c52c54fa17
tamerat47 <tameratmekonnen70@gmail.com>
2022-07-05 00:28:36 +0300

    The dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog data
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: who owns the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
