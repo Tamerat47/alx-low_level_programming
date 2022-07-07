bd1c3e9cb46269598b49e0144e9ba1b402e7c23e
tamerat47 <tameratmekonnen70@gmail.com>
2022-07-08 00:47:04 +0300

    fix sum it all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i = 0, sum = 0;
 
 	if (n == 0)
+	{
 		return (0);
+	}
 
 	va_start(var, n);
 
 	while (i < n)
+	{
 		sum += va_arg(var, int);
 		i++;
+	}
 
 	va_end(var);
 
