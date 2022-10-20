3bb5a8d011f3bd6dd6fa4ea83419a8dec99036bd
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-20 05:43:52 +0300

    natural $

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,13 @@
 void print_to_98(int n)
 {
 	if (n > 98)
-	{
-		while (n > 98)
-			printf("%d, ", n--);
-	}
+		printf("%d, ", n--);
+		continue;
 	else if (n == 98)
 		printf("%d\n", n);
 	else if (n < 98)
-	{
 		printf("%d, ", n--);
-	}
+		continue;
 	else
 		printf("%d\n", n);
 
