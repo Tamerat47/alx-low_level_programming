99798011b8aead31c3b724db684c75ea86bf9f97
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-20 03:52:15 +0300

    add two

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * add - adds two integer
+ *
+ * @a: is an integer
+ * @b: is an integer
+ */
+int add(int a, int b);
+{
+	int a, b, c;
+	{
+		c = a * b;
+
+		if (c <= 9)
+			putchar(c)
+		else
+			putchar((prod / 10) + '0');
+
+			putchar((prod % 10) + '0');
+	}
+}
