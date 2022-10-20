eddb89a7c4e01a325d0ec77a22285cb55c98631b
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-20 05:53:36 +0300

    n n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,15 @@
 void print_to_98(int n)
 {
 	if (n > 98)
-		printf("%d, ", n--);
-		continue;
-	else if (n == 98)
-		printf("%d\n", n);
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);\
+	}
 	else if (n < 98)
+	{
 		printf("%d, ", n--);
-		continue;
+	}
 	else
 		printf("%d\n", n);
 
