c4e8b44c04bbec28432bd3b37dc1aca4c42888c4
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-20 03:59:27 +0300

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,19 +2,17 @@
 /*
  * add - adds two integer
  *
- * @a: is an integer
- * @b: is an integer
  */
-int add(int a, int b)
+int add(int int)
 {
 	int a, b, c;
 
 		c = a * b;
 
 		if (c <= 9)
-			_putchar(c)
+			_putchar(c);
 		else
-			_putchar((prod / 10) + '0');
+			_putchar((c / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+			_putchar((c % 10) + '0');
 }
