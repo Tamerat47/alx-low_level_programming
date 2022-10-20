a828839cc862a8119c9025bc421acb5b7a7d2219
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-20 04:38:31 +0300

    natural number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print all natural number
+ *
+ * @n: number of natural number
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		if (n > 98)
+			printf("%d, ");
+			n--;
+			printf("%d\n", n);
+	}
+
+	else
+	{
+		if (n < 98)
+			printf("%d, ");
+			n++;
+			printf("%d\n", n);
+	}
+}
+
