6d61107ff1b0c384032737641de6a603528ac270
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-22 02:52:07 +0300

    trian print

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,9 +8,12 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = size; i >= 0; i--)
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = size - 1; i > 0; i--)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j < i - 1; j++)
 		{
 			_putchar(' ');
 		}
