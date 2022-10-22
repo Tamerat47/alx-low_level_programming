fb4152a872733bd069f1928037738cb03e06d6a6
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-22 04:19:54 +0300

    integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * print_number - Prints an integer.
+  * @n: integer
+  *
+  * Return: 0
+  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+		return (n);
+	}
+
+	if ((n == 0) && (n % 10 == 0)
+		return (n);
+}
