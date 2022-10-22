a418f7f5e2cd4ff50ef7f8adf52a0f03199fdcc5
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-22 03:52:49 +0300

    prime faactor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+/**
+  * main - Prints largest prime factor of the number 612852475143
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	long i, lf;
+	long a = 1231952;
+
+	for (i = 1; i <= (sqrt(a)); i++)
+	{
+		if (a % i == 0)
+		{
+			lf = a / i;
+		}
+	}
+
+	printf("%ld\n", lf);
+
+	return (0);
+}
