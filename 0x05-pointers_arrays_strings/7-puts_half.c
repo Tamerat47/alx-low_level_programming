bebe2d65711580cf667b018021670f973fc5415d
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-25 00:56:38 +0300

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ *
+ * @str: is a string
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+
+
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+	}
+	else
+	{
+		j = (i - 1) / 2;
+		j++;
+	}
+	for (; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
