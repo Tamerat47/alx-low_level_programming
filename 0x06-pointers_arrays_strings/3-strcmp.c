c77f470c512317284e6b8c9186011905eca98eda
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-26 21:45:45 +0300

    fix fix hot

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, dff;
+	int i, dff = 0;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0';)
 	{
-		if (s1[i] != s2[i])
-			dff = (*s1 - *s2);
-		else
-			dff = (*s1 - *s2);
+		dff = *s1 - *s2;
+		i++;
 	}
 	return (dff);
 }
