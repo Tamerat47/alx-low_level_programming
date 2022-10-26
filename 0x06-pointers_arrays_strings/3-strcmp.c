50bdcd5f379b8a3c5110a7a9aa8cfa3d079914e1
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-26 21:29:08 +0300

    fix compare hot

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,19 +2,21 @@
 /**
  * _strcmp - a function that compare's two string
  * @s1: a pointer to the first string
- * @s2: a pointer to the first string
+ * @s2: a pointer to the second string
  *
  * Return: print the difference
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
+	int i, dff;
 
-	while (s1[i] && s2[j] && s2[i] == s2[j])
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		i++;
-		j++;
+		if (s1[i] != s2[i])
+			dff = (*s1 - *s2);
+		else
+			dff = (*s1 - *s2);
 	}
-	return (*s1 - *s2);
+	return (dff);
 }
