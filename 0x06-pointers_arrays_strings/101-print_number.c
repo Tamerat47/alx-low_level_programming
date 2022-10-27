0e008ae57f5c9623d74c16757aad6a5240b4676c
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-28 02:45:54 +0300

    integer putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * print_number - Prints an integer.
@@ -8,18 +8,13 @@
   */
 void print_number(int n)
 {
-	if (n < 0)
+	if( n < 0)
 	{
-		n *= -1;
 		_putchar('-');
-		continue;
+		n = -n;
 	}
-
-	if (n != 0)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
+	
+	_putchar(n % 10 + '0');
 }
