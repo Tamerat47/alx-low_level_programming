907d221784a6142416e90fffbc764f7acc98dca2
tamerat47 <tameratmekonnen70@gmail.com>
2022-10-28 03:11:34 +0300

    late night fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,17 @@
   */
 void print_number(int n)
 {
+	int i;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
+	i = (n / 10);
 
-	_putchar((unsigned int) n % 10 + '0');
+	if (i != 0)
+		print_number(i);
+
+	_putchar(n % 10 + '0');
 }
